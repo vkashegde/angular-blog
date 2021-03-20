921ec8982c24776a1be3c9d376637604f48a34d1
{IamVks} <{vkshegde11@gmail.com}>
2021-03-20 21:34:34 +0530

    backend email validation added

--- models/user.js
+++ models/user.js
@@ -1,13 +1,49 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 const { Schema } = mongoose;
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt-nodejs");
+
+const emailLengthChecker = (email) => {
+  if (!email) {
+    return false;
+  } else {
+    if (email.length < 5 || email.length > 30) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+};
+
+const validEmailChecker = (email) => {
+  if (!email) {
+    return false;
+  } else {
+    const regExp = new RegExp(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+    return regExp.test(email);
+  }
+};
+
+const emailValidators = [
+  {
+    validator: emailLengthChecker,
+    message: "E-mail must be at least 5 characters but no more than 30",
+  },
+  {
+    validator: validEmailChecker,
+    message: "Must be a valid email",
+  },
+];
+
 const userSchema = new Schema({
   email: {
     type: String,
     required: true,
     unique: true,
     lowercase: true,
+    validate: emailValidators,
   },
   username: {
     type: String,
@@ -21,14 +57,18 @@ const userSchema = new Schema({
   },
 });
 
-// userSchema.pre("save", (next) => {
-//   if (!this.isModified("password")) return next();
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
 
-//   bcrypt.hash(this.password, null, null, (err, hash) => {
-//     if (err) return next(err);
-//     this.password = hash;
-//     next();
-//   });
-// });
+  bcrypt.hash(this.password, null, null, (err, hash) => {
+    if (err) return next(err);
+    this.password = hash;
+    next();
+  });
+});
+
+userSchema.methods.comparePassword = (password) => {
+  return bcrypt.compareSync(password, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
